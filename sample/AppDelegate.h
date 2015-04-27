3aa913002604b6bf66630da0d75bb67f40f1931f
Truong Dinh Tam <truong.tam@mulodo.com>
2015-04-27 19:55:00 +0900

    Initial Commit

---
+++ sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sample
+//
+//  Created by Truong Dinh Tam on 2015/04/27.
+//  Copyright (c) 2015年 Truong Dinh Tam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sample
+//
+//  Created by Truong Dinh Tam on 2015/04/27.
+//  Copyright (c) 2015年 Truong Dinh Tam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
